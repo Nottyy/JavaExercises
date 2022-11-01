d12fe7957d4ecaa2b0e638c480624b52baa45f7f
Antonio <antonio.marinovvvv@gmail.com>
2022-11-01 15:09:55 +0200

    3 Exercises Java

--- Last Digit Checker/src/Main.java
+++ Last Digit Checker/src/Main.java
@@ -4,9 +4,8 @@ public class Main {
 // Each number should be within the range of 10 (inclusive) - 1000 (inclusive). If one of the numbers is not within the range, the method should return false.
 // The mehod should return true if at least two of the numbers share the same rightmost digit; otherwise, it should return false.
         boolean result = hasSameLastDigit(19, 23, 34);
-
         System.out.println(result);
-
+        System.out.println(result + "sss");
     }
 
     public static boolean hasSameLastDigit(int num1, int num2, int num3){
