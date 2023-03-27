6f5b3ef5239f91622b0032e7a84ec9994e41de37
Antonio <antonio.marinovvvv@gmail.com>
2023-03-27 11:07:23 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	OOPGenericsLeagues/out/production/OOPGenericsLeagues/ITeam.class
    #	OOPGenericsLeagues/out/production/OOPGenericsLeagues/Main.class
    #	OOPGenericsLeagues/out/production/OOPGenericsLeagues/Team.class
    #	OOPGenericsLeagues/src/Main.java

--- OOP/InvestmentEarnings/src/Main.java
+++ OOP/InvestmentEarnings/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello mu!");
         System.out.println("Hello world!");
+        System.out.println("Hello s!");
     }
 }
