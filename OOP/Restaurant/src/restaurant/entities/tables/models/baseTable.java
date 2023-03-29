b1010813de44317520e4a40771c09381180a487b
Antonio <antonio.marinovvvv@gmail.com>
2023-03-29 22:59:12 +0300

    baseTable - bill, clear, orderBeverage, orderHealthyFood, reserve

---
+++ OOP/Restaurant/src/restaurant/entities/tables/models/baseTable.java
@@ -0,0 +1,121 @@
+package restaurant.entities.tables.models;
+
+import restaurant.common.ExceptionMessages;
+import restaurant.entities.drinks.interfaces.Beverages;
+import restaurant.entities.healthyFoods.interfaces.HealthyFood;
+import restaurant.entities.tables.interfaces.Table;
+
+import java.util.HashMap;
+
+public abstract class baseTable implements Table {
+    private int number;
+    private int size;
+    private double pricePerPerson;
+    private int numberOfPeople;
+
+    private boolean isReservedTable;
+    private int bill;
+    private HashMap<HealthyFood, Integer> healthyFoods;
+    private HashMap<Beverages, Integer> beverages;
+
+    public baseTable(int number, int size, double pricePerPerson) {
+        this.number = number;
+        this.setSize(size);
+        this.pricePerPerson = pricePerPerson;
+        this.isReservedTable = false;
+        this.numberOfPeople = 0;
+        this.bill = 0;
+        this.healthyFoods = new HashMap<>();
+        this.beverages = new HashMap<>();
+    }
+
+    @Override
+    public int getTableNumber() {
+        return this.number;
+    }
+
+    @Override
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setSize(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_TABLE_SIZE);
+        }
+        this.size = size;
+    }
+
+    @Override
+    public int numberOfPeople() {
+        return this.numberOfPeople;
+    }
+
+    @Override
+    public void reserve(int numberOfPeople) {
+        if (numberOfPeople <= 0) {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_NUMBER_OF_PEOPLE);
+        }
+
+        this.isReservedTable = true;
+        this.numberOfPeople = numberOfPeople;
+    }
+
+    @Override
+    public boolean isReservedTable() {
+        return this.isReservedTable;
+    }
+
+    @Override
+    public double pricePerPerson() {
+        return this.pricePerPerson;
+    }
+
+    @Override
+    public double allPeople() {
+        return this.numberOfPeople * pricePerPerson;
+    }
+
+    @Override
+    public void orderHealthy(HealthyFood food) {
+        if (this.healthyFoods.containsKey(food) == false){
+            this.healthyFoods.put(food, 1);
+        }
+        else{
+            this.healthyFoods.put(food, this.healthyFoods.get(food) + 1);
+        }
+
+        this.bill += food.getPrice();
+    }
+
+    @Override
+    public void orderBeverages(Beverages beverages) {
+        if (this.beverages.containsKey(beverages) == false){
+            this.beverages.put(beverages, 1);
+        }
+        else{
+            this.beverages.put(beverages, this.beverages.get(beverages) + 1);
+        }
+
+        this.bill += beverages.getPrice();
+    }
+
+    @Override
+    public double bill() {
+        return this.bill + this.allPeople();
+    }
+
+    @Override
+    public void clear() {
+        this.beverages.clear();
+        this.healthyFoods.clear();
+        this.bill = 0;
+        this.numberOfPeople = 0;
+        this.isReservedTable = false;
+    }
+
+    @Override
+    public String tableInformation() {
+        return null;
+    }
+}
