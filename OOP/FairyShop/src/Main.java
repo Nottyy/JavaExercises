9ec2571a0c38934d6838ddfd9a8eab2cf9586fe5
Antonio <antonio.marinovvvv@gmail.com>
2023-04-02 16:30:50 +0300

    InstrumentImpl class

--- OOP/FairyShop/src/Main.java
+++ OOP/FairyShop/src/Main.java
@@ -1,9 +1,6 @@
-import fairyShop.core.Engine;
-import fairyShop.core.EngineImpl;
-
 public class Main {
     public static void main(String[] args) {
-        Engine engine = new EngineImpl();
-        engine.run();
+//        Engine engine = new EngineImpl();
+//        engine.run();
     }
 }
