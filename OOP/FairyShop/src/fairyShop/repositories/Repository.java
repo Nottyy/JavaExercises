379583bdd6a2853df78fade0ff8dfb627414c00b
Antonio <antonio.marinovvvv@gmail.com>
2023-04-02 21:41:17 +0300

    PresentRepository

--- OOP/FairyShop/src/fairyShop/repositories/Repository.java
+++ OOP/FairyShop/src/fairyShop/repositories/Repository.java
@@ -3,8 +3,8 @@ package fairyShop.repositories;
 import java.util.Collection;
 
 public interface Repository<T> {
-    void add(T helper);
-    boolean remove(T helper);
+    void add(T model);
+    boolean remove(T model);
     T findByName(String name);
     Collection<T> getModels();
 }
